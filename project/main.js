63041b94c51a6185137574bf178d40de0a701e77
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-11-20 23:59:10 +0500

    Update main.js

--- project/main.js
+++ project/main.js
@@ -1,3 +1,19 @@
+// project 2 
 var ali = 'Hello ali, would you like to learn some Python today?';
 console.log(ali);
+// project 3
+// uppercase
+var hira = "SYEDA HIRA";
+console.log(hira);
+// lowercase
+var hira = "syeda hira";
+console.log(hira);
+// title case
+var hira = "Syeda Hira";
+console.log(hira);
+// project 4
+var motivationalquote = "Believe you can and you're halfway there.";
+var author = "Theodore Roosevelt";
+console.log("".concat(author, " once said, ").concat(motivationalquote));
+
 
