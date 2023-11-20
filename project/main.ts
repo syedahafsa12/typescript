380febee0233e429c43761bc0d1862bbcd6c3018
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-11-20 23:59:35 +0500

    Update main.ts

--- project/main.ts
+++ project/main.ts
@@ -1,3 +1,21 @@
+// project 2 
+
 let ali: string = 'Hello ali, would you like to learn some Python today?';
 console.log(ali);
 
+// project 3
+
+// uppercase
+var hira: string = "SYEDA HIRA"
+console.log (hira)
+// lowercase
+var hira: string = "syeda hira"
+console.log (hira)
+// title case
+var hira: string = "Syeda Hira"
+console.log (hira)
+
+// project 4
+let motivationalquote = "Believe you can and you're halfway there."
+let author = "Theodore Roosevelt"
+console.log ( `${author} once said, ${motivationalquote}`)
