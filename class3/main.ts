4be873f85aace05ab4b3233d7ac213acf21bc4f3
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-12-07 12:46:13 +0500

    Add files via upload

--- class3/main.ts
+++ class3/main.ts
@@ -1,1 +1,59 @@
+//  let username;
+//  username = "sir mustafa"
+//  username=456
+//  console.log(username)
 
+//  let studentGender = "Male"
+//  let studentGender = "Female"
+
+//  if(studentGender === "Male"){
+// console.log("Apki entry left side se hai")
+//  } else {
+//     console.log("Apki entry right side se hai")
+//  }
+
+// let salary =90000
+// let monthlyexpense = 80000
+// if (salary===40000){
+//     console.log("You need more money")
+// }else{
+//     console.log("Awesome")
+// }
+
+
+// if(studentGrade===50=<60){
+//     console.log("You are just pass")
+// }else if ( studentGrade=== 60>=70 ){
+//     console.log{"You got A grade"}
+// }else if ( studentGrade=== )
+
+// let studentGrade = 90;
+// let subject = "Maths";
+
+// let studentGrade= 20
+// let subject= "Maths"
+// if (studentGrade >= 50 && studentGrade <= 60) {
+// document.write("You are just pass");
+// } else if (studentGrade >= 61 && studentGrade <= 70) {
+//     document.write("You got A grade");
+// } else if (studentGrade >= 71 && studentGrade <= 80) {
+//     document.write("You got an A grade with distinction");
+// } else if (studentGrade >= 81 && studentGrade <= 90) {
+//     document.write("You got an A+ grade");
+// } else if (studentGrade >= 91 && studentGrade <= 100) {
+//     document.write("Congratulations! You got an A++ grade");
+// }else if(studentGrade<=49){
+//     document.write("You Failed");
+// } else {
+//     document.write("Invalid Score");
+// }
+
+console.log("Taking Decision")
+let budget=40000
+let cycle=45000
+let color="red"
+if( color === "blue" || color==="black" || color==="red" && cycle <= budget){
+console.log("Buy Cycle")
+}else{
+    console.log("build skills")
+}
