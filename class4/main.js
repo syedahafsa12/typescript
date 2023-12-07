bde15ea670720e2d97d064bf57550eb56ae4cb7c
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-12-07 12:46:53 +0500

    Add files via upload

---
+++ class4/main.js
@@ -0,0 +1,39 @@
+"use strict";
+// let firstName = "Hafsa"
+// let lastName = "Syed"
+// if (firstName === "Hamzah" || firstName === "Okasha"){
+//     console.log("You are not allowed")
+// }else{
+//     console.log("You are allowed")
+// }
+// function greet(name: string){
+//     console.log("Hello",name)
+// }
+// function sum(){
+//     console.log(2+2)
+// function sum(num1:number, num2:number) {
+//     // console.log(num1 + num2)
+//     return num1 + num2
+// }
+// function multiply(num1:number){
+//     console.log(num1*2)
+// }
+// let ans = sum(5,10)
+// console.log(ans)
+//  multiply(ans)
+// ARRAY
+// let fruits:string =["apple","banana","pineapple","orange"];
+// console.log(fruits)
+// let menu = ["kofta","kheer","biryani","gajar ka halwa"]
+// menu.pop()//remove from last
+// menu.push("chicken tikka") //add in last
+// menu.pop()
+// console.log(menu)
+// let pushedReturn = menu.push("Chicken Tikka","Chicken Karahi")
+// console.log(pushedReturn)
+// let shiftedElement = menu.shift()
+// console.log(shiftedElement)
+// let len = menu.unshift("Chicken Tikka")
+// console.log(len);
+let fruits = ["banana", "apple", "orange"];
+fruits.splice(2, 0, "lemon");
