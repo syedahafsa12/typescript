a7669f5706955b24ebee61db2a60d5906cfa8187
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-12-17 12:42:58 +0500

    Add files via upload

--- class5/main.ts
+++ class5/main.ts
@@ -1,1 +1,68 @@
+console.log("class5")
+
+
+//loops
+// for (let i = 0; i<6; i++){
+//     console.log("helloworld")
+// }
+// for (let i = 1; i<11;i++){
+// console.log(i)
+// }
+
+// const std = ["hafsa","hania","hurain"]
+// for (let i = 1; i<11;i++){
+//     console.log(std[2])
+// }
+
+//  const std = ["hafsa","hania","hurain"]
+//  for (let i = 0; i<3;i++){
+
+//      console.log(std[1])
+// }
+
+// const std = ["hafsa","hania","hurain"]
+// for (let i = 0; i<3;i++){
+
+//     console.log(`${std[1]}`)
+
+// }
+
+// for (let a =1;a<= 10; a++){
+//     let ans = a *2
+//     console.log("2","x",a,"=",ans)
+// }
+
+// let arr = ["a","b","c","d","e","f","g","h","i","j"]
+// for (let index = 0;index <10; index++){
+//     console.log(arr[index])
+// }
+
+
+// for (let i = 0; i<3;i++){
+//     console.log("parent loop",i)
+// break
+//     for (let j = 0;j<4;j++)
+// console.log("child loop")
+// }
+
+let video1 = [
+    {
+        title: "video1",
+        description: "video 1 description",
+        image: "https://image.com"
+    }
+{
+        title: "video2",
+        description: "video 1 description",
+        image: "https://image.com"
+    }
+]
+
+console.log(video1[1].description)
+
+
+
+
+
+
 
