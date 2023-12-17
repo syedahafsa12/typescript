888b08246f4d9d37ba30480e7262d245aa70dcea
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2023-12-17 16:58:52 +0500

    Add files via upload

--- Calculator/main.ts
+++ Calculator/main.ts
@@ -1,1 +1,23 @@
 
+
+var prompt = require('prompt-sync')();
+let n1 = parseInt(prompt('Please Enter your First Number:'))
+let n2 = parseInt(prompt('Please Enter your Second Number:'))
+let Operation = prompt("Write any 1 Operation from the following:(+,-,*,/):")
+if(Operation=="+"){
+    console.log(`Your Answer is ${n1} ${Operation} ${n2} = ${n1 + n2} `)
+}
+else if(Operation=="-"){
+    console.log(`Your Answer is ${n1} ${Operation} ${n2} = ${n1 - n2} `)
+}
+else if(Operation=="/"){
+    console.log(`Your Answer is ${n1} ${Operation} ${n2} = ${n1 / n2} `)
+}
+else if(Operation=="*"){
+    console.log(`Your Answer is ${n1} ${Operation} ${n2} = ${n1 * n2} `)
+}else{
+    console.log("Please select the correct operator")
+}
+
+export{};
+
