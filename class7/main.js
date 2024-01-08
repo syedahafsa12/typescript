a7f10899106f2a207d1e9d4917e6c2f850b63cf8
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2024-01-08 09:59:32 +0500

    Add files via upload

---
+++ class7/main.js
@@ -0,0 +1,68 @@
+// union let a:string | number = "Hafsa"
+// a = 10
+// a = "hi"
+//dot
+// let age:number = 10
+// age.toFixed()
+// let fname = "hafsa";
+// fname.toLocaleLowerCase
+//narrowing
+// let age:string|number;
+// age.
+export {};
+// let age:string |number = "22"
+// console.log(typeof age)
+// let age:string |number;
+// if (typeof age === "string") {
+//     age.toUpperCase
+// }else{
+//     age.
+// }
+// console.log(Math.random())
+// let newage = Math.random()> 0.5 ? 22:"khan"
+//wrong method
+// if (typeof newage === "string"){
+//     newage.toUpperCase()
+// }else {
+// newage.toFixed(2)
+// }
+//literal
+// let trafficlights:"yellow"|"red"|3 = "red"
+// let teacher :{
+//     name:string,
+//     salary:number,
+//     "full-name":string
+// }={
+//     name:"Hafsa",
+//     salary:20000,
+//     "full-name":"syeda hafsa"
+// }
+// teacher ["full-name"]
+// let teacher:{name:string,exp:number}
+// type student = string |boolean |number |undefined 
+// let a : student;
+// type student = {
+//     name:string,
+//     age:number
+// }
+// let student1:student = {
+//     name:"hafsa",
+//     age:13
+// }
+// type deal1 = {
+//     food:string,
+//     sweet:string
+// }
+// type deal2={
+//     food:string,
+//     drink:string
+// }
+// let hamzahorder: deal1={
+//     food:"biryani",
+//     sweet:"Gulab Jamun"
+// }
+// let okashaorder: deal2={
+//     food:"biryani",
+//     drink:"colanext"
+// }
+// hamzahorder = okashaorder
