2631e15a67dec9c33367dd748e9be6f437a1a33e
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2024-01-17 21:55:30 +0500

    Add files via upload

---
+++ class6/service.js
@@ -0,0 +1,14 @@
+"use strict";
+// export let f = "hi"
+// export let g = "hi"
+// export let h = "hi"
+// export let i = "hi"
+// export let j = "hi"
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.h = exports.j = void 0;
+let j = "hi";
+exports.j = j;
+let h = "hi";
+exports.h = h;
+let z = "hibye";
+exports.default = z;
---
+++ class6/service.ts
@@ -0,0 +1,13 @@
+// export let f = "hi"
+// export let g = "hi"
+// export let h = "hi"
+// export let i = "hi"
+// export let j = "hi"
+
+let j = "hi"
+let h = "hi"
+
+export{j,h}
+
+let z = "hibye"
+export default z;
