408d43062776587382789f7e7c287c217cb6eae5
Syeda Hafsa <142673057+syedahafsa12@users.noreply.github.com>
2024-01-19 10:03:47 +0500

    Add files via upload

--- word-counter/main.ts
+++ word-counter/main.ts
@@ -1,1 +1,22 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+function counter (para:string){
+let freewhitespace = para.replace(/\s/g,"")
+return freewhitespace.length
+}
+// console.log(counter('Hello hafsa'))
+async function start(){
+let q1 = await inquirer.prompt({
+    type:"input",
+    name:"userinputforcount",
+    message:"Enter text to count!"
+})
+console.log(`You have written:${q1.userinputforcount}`)
+
+console.log(`${chalk.greenBright('Your text count without spaces is')} ${chalk.redBright.bold(counter(q1.userinputforcount))}`)
+}
+
+start()
+
 
